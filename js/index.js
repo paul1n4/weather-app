6e3362be22f336ce64debe33beb508b6905b5f2f
paul1n4 <paulinevaleroc@gmail.com>
2022-11-26 08:20:20 -0500

    Add viewport.js, resize viewport with js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 import currentWeather from './current-weather.js'
+import { viewportSize } from './utils/viewport.js'
+
+const $app = document.querySelector('#app')
+const $loading = document.querySelector('#loading')
+
+viewportSize($app)
+viewportSize($loading)
 
 currentWeather()

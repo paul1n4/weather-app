874990d1907d5aa255663ffa0c8777f1e3a2b9fc
paul1n4 <paulinevaleroc@gmail.com>
2022-11-29 16:01:21 -0500

    Create tabs.js & change the name of the days

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import currentWeather from './current-weather.js'
 import { viewportSize } from './utils/viewport.js'
+import './tabs.js'
 
 const $app = document.querySelector('#app')
 const $loading = document.querySelector('#loading')

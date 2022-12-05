af42215e79cab8cd34ab24f5cac8c3170a0707e3
paul1n4 <paulinevaleroc@gmail.com>
2022-12-05 13:05:11 -0500

    Create dom.js, use append() & show tabPanel in each tabDay

--- js/weekly-weather.js
+++ js/weekly-weather.js
@@ -1,9 +1,34 @@
 import { getWeeklyWeather } from './services/weather.js'
 import { getLatLon } from './geolocation.js'
 import { formatWeekList } from './utils/format-data.js'
+import { createDOM } from './utils/dom.js'
 
-function configWeeklyWeather() {
+function tabPanelTemplate(id) {
+  return `
+    <div class="tabPanel" tabindex="0" aria-labelledby="tab-${id}">
+      <div class="dayWeather" id="dayWeather-${id}">
+        <ul style="color: white" class="dayWeather-list" id="dayWeather-list-${id}">
+          Tab Panel ${id}
+        </ul>
+      </div>
+    </div>
+  `
+}
 
+function createTabPanel(id) {
+  const $panel = createDOM(tabPanelTemplate(id))
+  if (id > 0) {
+    $panel.hidden = true
+  }
+  return $panel
+}
+
+function configWeeklyWeather(weeklist) {
+  const $container = document.querySelector('.weeklyWeather')
+  weeklist.forEach((day, index) => {
+    const $panel = createTabPanel(index)
+    $container.append($panel)
+  })
 }
 
 export default async function weeklyWeather() {
@@ -12,6 +37,5 @@ export default async function weeklyWeather() {
   const { isError: weeklyWeatherError, data: weather } = await getWeeklyWeather(lat, lon)
   if (weeklyWeatherError) return console.log('oh! ha ocurrido un error trayendo el pronóstico del clima');
   const weeklist = formatWeekList(weather.list)
-  debugger
-  configWeeklyWeather(weather)
+  configWeeklyWeather(weeklist)
 }
